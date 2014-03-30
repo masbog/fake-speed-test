add17dde97ff2b4030d7db4bf00aa1d64376e40a
Augusta Bogie <masbog@me.com>
2014-03-30 22:00:05 +0700

    first commit

---
+++ speedtest.pl
@@ -0,0 +1,118 @@
+//
+//  speedtest.pl
+//
+//  Created by Augusta Bogie on 3/30/14.
+//  Copyright (c) 2014 asepmoels. All rights reserved.
+//
+//  tweak up from another user at github.com https://github.com/gray/dotfiles/blob/master/bin/fake-speedtest.pl
+#!/usr/bin/env perl
+use strict;
+use warnings;
+
+use Carp qw(croak);
+use Digest::MD5 qw(md5_hex);
+use Geo::Distance;
+use Getopt::Long qw(:config auto_help);
+use LWP::UserAgent;
+use Pod::Usage;
+use XML::LibXML;
+
+GetOptions(
+    'download|d=f' => \my $down,
+    'upload|u=f'   => \my $up,
+    'ping|p=i'     => \my $ping,
+    'lat|lat=f'     => \my $clat,
+    'lon|lon=f'     => \my $clon,
+) or pod2usage(2);
+pod2usage(2) unless 3 == grep defined, $down, $up, $ping;
+for ($down, $up) {
+    pod2usage(2) unless 0 < $_ and 1_000 > $_;
+}
+
+my $ua = LWP::UserAgent->new(
+    agent => 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_8_2) AppleWebKit/536.26.17 (KHTML, like Gecko)  '
+           . 'Version/6.0.2 Safari/536.26.17',
+);
+
+my $time = time . sprintf "%03d", int rand(999);
+
+my $dom = parse_config("http://speedtest.net/speedtest-config.php?x=$time");
+my ($node) = $dom->findnodes('/settings/client');
+#my ($clat, $clon) = map { $node->findvalue("\@$_") } qw(lat lon);
+
+# Choose the closest server.
+# TODO: use AnyEvent::FastPing since flash client now chooses server by
+#   best ping time.
+$dom = parse_config("http://speedtest.net/speedtest-servers.php?x=$time");
+my $geo = Geo::Distance->new;
+my ($server, $distance);
+for my $node ($dom->findnodes('/settings/servers/server')) {
+    my ($id, $slat, $slon) = map { $node->findvalue("\@$_") } qw(id lat lon);
+    my $d = $geo->distance(mile => $clon, $clat, $slon, $slat);
+    if (not defined $distance or $d < $distance) {
+        $server   = $id;
+        $distance = $d;
+    }
+}
+die 'No server' unless $server;
+
+$_ *= 1_000 for ($up, $down);
+
+my $salt = '297aae72';
+my $hash = md5_hex(sprintf '%s-%s-%s-%s', $ping, $up, $down, $salt);
+my $res = $ua->post(
+    'http://www.speedtest.net/api/api.php',
+    referer => 'http://c.speedtest.net/flash/speedtest.swf',
+    content => [
+        startmode           => 'recommendedselect',
+        promo               => '',
+        upload              => $up,
+        accuracy            => 7 + int rand(20),  # ?
+        recommendedserverid => $server,
+        serverid            => $server,
+        ping                => $ping,
+        hash                => $hash,
+        download            => $down,
+	sponsor		    => 'MasBog Inc.',
+	carrier		    => 'MasBog',
+	isp		    => 'MasBog Inc.',
+    ]
+);
+die $res->dump if $res->is_error;
+
+my %data = map { split '=' } split '&', $res->decoded_content;
+die "Bad values rejected by SpeedTest.Net" unless $data{resultid};
+
+printf "http://speedtest.net/result/%s.png\n", $data{resultid};
+
+exit;
+
+
+sub parse_config {
+    my ($url) = @_;
+    my $res = $ua->get($url);
+    croak $res->status_line if $res->is_error;
+
+    my $xml = $res->decoded_content;
+    return eval { XML::LibXML->new(recover => 2)->parse_string($xml) };
+}
+
+__END__
+
+=head1 NAME
+
+fake-speedtest.pl
+
+=head1 SYNOPSIS
+
+  fake-speedtest.pl --down download --up upload --ping ping
+
+  Options:
+
+    -d --down  Download speed in Mb/s (0 < x < 1,000)
+    -u --up    Upload speed in Mb/s   (0 < x < 1,000)
+    -p --ping  Ping latency in ms
+    -lat --lat  Latitude server
+    -lon --lon  Longitude server
+
+=cut
